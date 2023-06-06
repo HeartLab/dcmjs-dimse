8df09b8e37e2de4e0cc4a07468705e23c61d4931
Pantelis Georgiadis <PantelisGeorgiadis@Gmail.com>
2023-06-06 12:33:09 +0300

    Fixed rebase misses
    (cherry picked from commit 17f8a88f79e79094976fa64b215d5099fb0b76b2)

--- src/Network.js
+++ src/Network.js
@@ -514,7 +514,7 @@ class Network extends AsyncEventEmitter {
     }
   }
 
-/**
+  /**
    * Process P-DATA-TF.
    * @method
    * @private
@@ -664,7 +664,7 @@ class Network extends AsyncEventEmitter {
               const dataset = new Dataset(
                 this.dimseStream.toBuffer(),
                 presentationContext.getAcceptedTransferSyntaxUid(),
-				this.datasetReadOptions
+                this.datasetReadOptions
               );
               this.dimse.setDataset(dataset);
               this._performDimse(presentationContext, this.dimse);
